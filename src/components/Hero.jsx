30c59d7b8e23b0a27d8dc549b949f116f23a1b53
Abby F <abby_faigao@yahoo.com>
2022-06-01 03:26:37 -0700

    Fix hero component video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,17 +18,14 @@ const Hero = () => {
         </button>
       </div>
       <video
-        autoplay
+        playsInline
+        autoPlay
         loop
         muted
-        class="absolute w-auto z-10 min-w-full min-h-full max-w-none"
-      >
-        <source
-          src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+        src={Video}
+        type="video/mp4"
+        className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+      />
     </header>
   );
 };
