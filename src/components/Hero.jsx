6b1e64cc31a9b5eff74e226d49e0fae9ab979e79
Abby F <abby_faigao@yahoo.com>
2022-05-31 21:54:36 -0700

    Add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Video from "../assets/video.mp4";
+
+const Hero = () => {
+  return (
+    <header className=" relative flex items-center justify-center h-screen mb-12 overflow-hidden">
+      <div className="relative z-30 p-5 text-2xl text-white  bg-opacity-50">
+        <p className="text-cyan-600  font-bold p-2">GROW YOUR BUSINESS</p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Manage your data.
+        </h1>
+        <p className="md:text-2xl text-xl font-bold text-white ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+          suscipit?
+        </p>
+        <button className="bg-[#FFC40A] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black hover:bg-[#F8DE7E]">
+          Get Started
+        </button>
+      </div>
+      <video
+        playsInline
+        autoPlay
+        loop
+        muted
+        src={Video}
+        type="video/mp4"
+        className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
+      />
+    </header>
+  );
+};
+
+export default Hero;
