2f3b8510092db46333c355b4053f67c92f997d87
Abby F <abby_faigao@yahoo.com>
2022-06-03 23:29:08 -0700

    Add contact form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import Contact from "./components/Contact";
-// import About from "./components/About";
-// import Footer from "./components/Footer";
+import About from "./components/About";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-// import Services from "./components/Services";
-// import Team from "./components/Team";
+import Services from "./components/Services";
+import Team from "./components/Team";
 
 function App() {
   return (
     <>
-      {/* <Navbar />
+      <Navbar />
       <Hero />
       <About />
       <Services />
       <Team />
-      <Footer /> */}
+      <Footer />
       <Contact />
     </>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,23 +3,164 @@ import Typed from "react-typed";
 
 const Contact = () => {
   return (
-    <section className="bg-[#2F2E2C] w-full h-full fixed overflow-hidden top-0 bottom-0 left-0 right-0 ">
-      <div className="h-full flex justify-center sm:max-h-[80%]">
-        <h1 className="py-8 text-end text-[#FFC40A] font-bold text-4xl sm:ml-4 sm:mt-2">
-          Day1 Consulting
-        </h1>
-        <div>
-          <form action="">
-            <h1>Need pricing?</h1>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="Enter name.." />
-            <label htmlFor="email">Email</label>
-            <input type="text" placeholder="Enter email..." />
-          </form>
-          <button type="submit">Submit</button>
+    <>
+      <section className="text-gray-700 relative bg-[#679BF1] ">
+        <div className="px-3 py-24 mx-auto">
+          <h1 className=" ml-8  font-bold md:text-4xl sm:text-3xl text-[#F6C40A]">
+            Day1 Consulting
+          </h1>
+          <div className="flex flex-col text-center w-full mb-12">
+            <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-7 mt-20">
+              Contact us for
+              <Typed
+                className="md:text-5xl sm:text-3xl text-xl font-bold md:pl-4 pl-2 text-[#F8DE7E]"
+                strings={["Pricing", "Consultation", "General Inquiries"]}
+                typeSpeed={100}
+                backSpeed={90}
+                loop
+              />
+            </h1>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
+              praesentium dolore perspiciatis nisi consequuntur et nihil,
+              perferendis eius assumenda facilis.
+            </p>
+          </div>
+          <div className="lg:w-1/2 md:w-2/3 mx-auto w-full">
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    htmlFor="name"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+
+              <div className="p-2 w-1/2">
+                <div className="relative">
+                  <label
+                    htmlFor="email"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                  />
+                </div>
+              </div>
+
+              <div className="p-2 w-full">
+                <div className="relative">
+                  <label
+                    for="message"
+                    className="leading-7 text-sm text-gray-600"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    className="w-full h-[300px] bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                  ></textarea>
+                  {/* w-full bg-gray-100 rounded border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out */}
+                </div>
+              </div>
+
+              <div className="p-2 w-full">
+                <button className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Button
+                </button>
+              </div>
+
+              <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                {" "}
+                <a className="text-indigo-500">example@email.com</a>
+                <p className="leading-normal my-5">
+                  505 Burrard St.
+                  <br />
+                  Vancouver, BC V7X 1M5
+                </p>
+                <span className="inline-flex">
+                  {/* <a className="text-gray-500">
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
+                  </a>
+
+                  <a className="ml-4 text-gray-500">
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                    </svg>
+                  </a>
+
+                  <a className="ml-4 text-gray-500">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <rect
+                        width="20"
+                        height="20"
+                        x="2"
+                        y="2"
+                        rx="5"
+                        ry="5"
+                      ></rect>
+                      <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
+                  </a>
+
+                  <a className="ml-4 text-gray-500">
+                    <svg
+                      fill="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                    </svg>
+                  </a> */}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
