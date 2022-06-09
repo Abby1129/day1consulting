4d5a11c660720d3b57b0d743b3da1c9cc000aea1
Abby F <abby_faigao@yahoo.com>
2022-06-08 21:37:28 -0700

    remove link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import Contact from "./pages/contact";
 
